2cdf85d5b8217d71ce1ee804f5dce7582835a5b2
MarlenaRowa <marlena.pirchl@rowa-automation.at>
2022-06-24 08:50:56 +0200

    Add project files.

---
+++ Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Post
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+        public int versionOnRobot { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using ConsoleApp1;
+using Nancy.Json;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace RestApiTest
+
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+
+        {
+            //for (; ; )
+                getdata();
+        }
+
+        public static void getdata()
+
+        {
+            var client = new RestClient("http://localhost:5011/");
+            var request = new RestRequest("/test");
+            var response = client.Execute(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                string rawResponse = response.Content; 
+                Console.WriteLine(rawResponse);
+                DataSub1 datumDto = new JavaScriptSerializer().Deserialize<DataSub1>(rawResponse); ;
+                foreach (var item in datumDto.data)
+                {
+                    Console.WriteLine("Co wyszlo:" + item.id);
+                }
+            }
+            else
+            {
+                Console.Write("chuj"); 
+            }
+
+        }
+
+        public class DataSub1
+        {
+            public List<DatumDto> data { get; set; }
+
+        }
+        public class DatumDto
+        {
+            public string id { get; set; }
+        }
+
+            public static void Postdata()
+        {
+            var client = new RestClient("http://localhost:5010/");
+    ;
+            var request = new RestRequest("v2/entities", Method.Post);
+            request.AddHeader("Content-Type", "application/json");
+            var body = new Post {
+
+                id = "dsdf",
+                name = "asdas",
+                type = "Program",
+                versionOnRobot = 1
+            };
+            request.AddJsonBody(body);
+
+            var response1 = client.Post(request);
+            Console.WriteLine(response1.Content);
+
+        }
+    }
+
+
+
+
+}
